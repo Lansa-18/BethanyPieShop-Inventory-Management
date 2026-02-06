e96edfbdb649d8720f486b84a0d16c870e75303c
Mapelujo Abdulkareem <olamideiyanda18@gmail.com>
2026-02-06 22:13:08 +0100

    Update program.cs to instantiate objects from defined classes

--- Domain/General/Price.cs
+++ Domain/General/Price.cs
@@ -7,6 +7,11 @@ public class Price
     public double ItemPrice { set; get; }
     public Currency Currency { get; set; }
 
+    public Price(double price, Currency currency) {
+        ItemPrice = price;
+        Currency = currency;
+    }
+
     public override string ToString()
     {
         return $"{ItemPrice} {Currency}";
--- Program.cs
+++ Program.cs
@@ -1,2 +1,47 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using BethanysPieShop.InventoryManagement.Domain.General;
+using BethanysPieShop.InventoryManagement.Domain.ProductManagement;
+
+PrintWelcome();
+
+//Create inventory
+Price samplePrice = new Price(10, Currency.Euro);
+Product p1 = new Product(1, "Sugar", "Lorem Ipsum", samplePrice, UnitType.PerKg, 100);
+
+p1.IncreaseStock();
+p1.Description = "Sample description";
+
+var p2 = new Product(2, "Cake decorations", "Lorem Ipsum", )
+
+
+#region Layout
+
+static void PrintWelcome()
+{
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(@"
+    ()()()()()()   ____       _   _                       _       _____ _         _____ _                                        
+    |\         |  |  _ \     | | | |                     ( )     |  __ (_)       / ____| |                                         ()()()()()()
+    |.\. . . . |  | |_) | ___| |_| |__   __ _ _ __  _   _|/ ___  | |__) |  ___  | (___ | |__   ___  _ __                           |\         |
+    \'.\       |  |  _ < / _ \ __| '_ \ / _` | '_ \| | | | / __| |  ___/ |/ _ \  \___ \| '_ \ / _ \| '_ \                          |.\. . . . |
+     \.:\ . . .|  | |_) |  __/ |_| | | | (_| | | | | |_| | \__ \ | |   | |  __/  ____) | | | | (_) | |_) |                         \'.\       |
+      \'o\     |  |____/ \___|\__|_| |_|\__,_|_| |_|\__, | |___/ |_|__ |_|\___| |_____/|_| |_|\___/| .__/                    _      \.:\ . . .|
+       \.'\. . |  |_   _|                    | |     __/ |         |  \/  |                        | |                      | |      \'o\     |
+        \'.\   |    | |  _ ____   _____ _ __ | |_ __|___/__ _   _  | \  / | __ _ _ __   __ _  __ _ |_|_ _ __ ___   ___ _ __ | |_      \.'\. . |
+         \'`\ .|    | | | '_ \ \ / / _ \ '_ \| __/ _ \| '__| | | | | |\/| |/ _` | '_ \ / _` |/ _` |/ _ \ '_ ` _ \ / _ \ '_ \| __|      \'.\   |
+          \.'\ |   _| |_| | | \ V /  __/ | | | || (_) | |  | |_| | | |  | | (_| | | | | (_| | (_| |  __/ | | | | |  __/ | | | |_        \'`\ .|
+           \__\|  |_____|_| |_|\_/ \___|_| |_|\__\___/|_|   \__, | |_|  |_|\__,_|_| |_|\__,_|\__, |\___|_| |_| |_|\___|_| |_|\__|        \.'\ |
+                                                             __/ |                            __/ |                                       \__\|
+                                                            |___/                            |___/                               
+    ");
+
+    Console.ResetColor();
+
+    Console.WriteLine("Press enter key to start logging in!");
+
+    //accepting enter here
+    Console.ReadLine();
+
+    Console.Clear();
+}
+#endregion
